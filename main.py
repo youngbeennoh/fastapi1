93559e70e9b31db4a1a70b53b251087a6ffb5b67
nohyb <nohyb@ais-info.co.jp>
2025-11-07 14:58:55 +0900

    change

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-from fastapi import FastAPI , Query;
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-import os
 import uvicorn
+import os
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware,
-                    allow_origins=["*"],
-                    allow_credentials=False,
-                    allow_methods=["*"],
-                    allow_headers=["*"],
-                    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
 async def read_root():
-    return {"Hello" : "World"}
+    return {"Hello": "World"} 
 
+@app.get('/user')
+async def read_user():
+    return {"BACK"}
 
-
-@app.get("/order/apple")
-async def read_fruit(color:str = Query(max_length=5)):
+@app.get('/order/apple')
+async def read_apple(color: str = Query(max_length=5)):
     if color == "red":
         ee = "🍎"
     else:
         ee = "🍏"
-    return {"msg": ee+"がご注文されました"}
 
-@app.get('/pineapple')
-async def read_pineapple():
-    return "🍍がご注文されました"
+    #  ee = "🍎"  if  color == "red" else "🍏"
+    return {"msg":ee+"が注文されました。"}
 
-if __name__== "__main__":
-    port = int(os.environ.get("PORT, 8000"))
+# @app.get('/order/apple/blue')
+# async def read_apple():
+#     ee = "🍏"
+#     return {"msg",ee+"が注文されました。"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
